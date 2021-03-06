5ae336d3fdf9c1027b80dafc876c5396864162a4
ForMe1970 <ForMe1970@163.com>
2021-03-06 21:06:31 +0800

    用户显示界面

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        //http://www.gravatar.com/avatar/$hash?s=$size
+        return "";
+    }
 }
