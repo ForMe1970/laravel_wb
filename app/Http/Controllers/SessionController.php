e3110d9c6287f8ff5ba02a38b07a2e79fa506184
ForMe1970 <ForMe1970@163.com>
2021-03-10 23:15:33 +0800

    创建会话

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('session.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credntials = $this->validate($request, [
+            'email' => 'email|required|max:25',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credntials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+         }
+    }
+}
