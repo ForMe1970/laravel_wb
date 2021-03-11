51a73ce44260b6f11fc460efbcf32efbb74d81c2
ForMe1970 <ForMe1970@163.com>
2021-03-11 22:03:24 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,27 @@ class UsersController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+        session()->flash('success', '个人资料更行成功！');
+        return redirect()->route('users.show', $user->id);
+    }
 }
